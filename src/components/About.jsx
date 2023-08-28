c1cf5a860ff0277f3980195ab2d04c1537f55de0
Joshua Grimard <jgrimard22@gmail.com>
2023-08-28 09:09:17 -0600

    Updated the About page.

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,5 @@
 import { Wrapper } from '../assets/wrappers/AboutComponent';
-import aboutImg from '../assets/images/hero-img2.jpg';
+import aboutImg from '../assets/images/hero-img5.jpg';
 import { Link } from 'react-router-dom';
 import { SectionTitle } from '../components';
 
@@ -11,15 +11,15 @@ const About = () => {
                 <img src={aboutImg} alt='About Hero Image' className='hero-photo'/>
             </article>
             <article className='about-info'>
-                <SectionTitle className='sectioncomponent'>about</SectionTitle>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
-                    Fuga nemo perspiciatis iusto inventore 
-                    ullam eveniet. Dolorem sint repellendus recusandae expedita reprehenderit 
-                    illo molestiae ducimus, perspiciatis suscipit voluptatem itaque consequatur ut.
-                    Fuga nemo perspiciatis iusto inventore 
-                    ullam eveniet. Dolorem sint repellendus recusandae expedita reprehenderit 
-                    illo molestiae ducimus, perspiciatis suscipit voluptatem itaque consequatur ut.
-                </p>
+                <SectionTitle className='sectioncomponent'>who am i?</SectionTitle>
+                <p>
+                I'm a Saskatchewan-based, Indigenous Metis Full-Stack Developer born and raised in Prince Albert. 
+                I graduated from the Computer Systems Technology program offered by SaskPolytech in 2021 and have been working
+                in the tech industry ever since. I have worked on a couple big projects for the City of Prince Albert and the 
+                Restorative Action Program located in Saskatoon, SK, just to name a few. In my spare time, I love to research and
+                 play around with Machine Learning applications, especially Stable Diffusion. Outside of computer related things, I 
+                 consider myself a foodie and love trying out new recipes!
+                </p> 
                 <Link to='about' className='btn'>about me</Link>
             </article>
         </div>
